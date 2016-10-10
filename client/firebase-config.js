b9abf679d5e94eb95e7a462b07a0a3fd8dd23075
Elliot Cheung <ezcheung>
2016-10-10 16:45:19 -0500

    Set up firebase snippets

---
+++ client/firebase-config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDU3qympK0n7Ts6ZsgRB2CqE39fUu_W2I8",
+  authDomain: "stately-shower.firebaseapp.com",
+  databaseURL: "https://stately-shower.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "507309554511"
+};
+firebase.initializeApp(config);
