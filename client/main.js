c7ebb659c80a0d7f945b46c41f6e19427f567707
Elliot Cheung <ezcheung>
2016-10-10 18:50:02 -0500

    Set up basic files

---
+++ client/main.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+
+import React from 'react';
+import MainView from './views/mainView.js';
+import { Router, Route, Link } from 'react-router';
+
+ReactDOM.render(<App />, document.getElementById('app'));
