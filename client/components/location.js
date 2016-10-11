6cc8a4cd2f9e283eb6e613e4bc30d2669b72c231
Elliot Cheung <ezcheung>
2016-10-11 16:05:10 -0500

    Location now displays correctly

--- client/components/location.js
+++ client/components/location.js
@@ -11,9 +11,12 @@ export default class Location extends React.Component {
     this.state = {
       location: this.props.loc,
       occupied: false,
-      inUser: null
+      inUser: null,
+      startTime: null,
+      currentTime: Date.now()
     }
     this.dbLoc = firebase.database().ref(`${this.props.loc}`);
+    setInterval(() => this.setState({currentTime: Date.now()}), 1000);
   }
 
   componentWillMount() {
@@ -21,7 +24,8 @@ export default class Location extends React.Component {
       console.log("locdata.val: ", locData.val());
       this.setState({
         occupied: locData.val().occupied,
-        inUser: locData.val().user
+        inUser: locData.val().user,
+        startTime: locData.val().startTime
       })
     })
   }
@@ -44,8 +48,35 @@ export default class Location extends React.Component {
     }
   }
 
+  formatTimeDiff() {
+    let diff = this.state.currentTime - this.state.startTime;
+    if(diff <= 0) {
+      return "0:00";
+    }
+    let secs = Math.floor(diff/1000);
+    let mins = Math.floor(secs/60);
+    secs = secs % 60;
+    if(secs < 10) {
+      secs = '0' + secs;
+    }
+    return `${mins}:${secs}`;
+  }
+
   occupantDisplay() {
-    
+    if(!this.state.inUser) {
+      return null;
+    } else {
+      return (
+        <div>
+          <div className="occupant">
+          {`Occupied by: ${this.state.inUser.name}`}
+          </div>
+          <div className="duration">
+          {this.formatTimeDiff()}
+          </div>
+        </div>
+      )
+    }
   }
 
   render() {
@@ -54,6 +85,7 @@ export default class Location extends React.Component {
         <h1>{this.props.loc}</h1>
         <div className="locControls">
           {this.buttonSelect()}
+          {this.occupantDisplay()}
         </div>
       </div>
     )
