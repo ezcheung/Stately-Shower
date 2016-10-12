e106011370d62af51cc34c544348d1547f8c653e
Elliot Cheung <ezcheung>
2016-10-11 19:30:38 -0500

    First version deployed

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var browserify = require('browserify-middleware');
+var firebase = require('firebase');
 app.use(express.static(path.join(__dirname, "../client/public")));
 
-app.get('/app-bundle.js',
+app.get('/bundle.js',
  browserify('./client/main.js', {
     transform: [ [ require('babelify'), { presets: ["es2015", "react"] } ] ]
   })
