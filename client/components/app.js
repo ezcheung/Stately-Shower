3cf1c82a2bd5928f6590d39924a3bf7c08252575
Elliot Cheung <elliotccheung@gmail.com>
2016-10-31 20:53:52 +0000

    Added welcome message if not in a location

--- client/components/app.js
+++ client/components/app.js
@@ -69,7 +69,7 @@ export default class App extends React.Component {
 
   currentlyIn () {
     if (!this.state.userIsIn) {
-      return <h2 className="hidden">Hi there</h2>;
+      return <h2>{`Welcome, ${this.state.currentUser.displayName.split(' ')[0]}!`}</h2>;
     }
     return <h2>{`You are in the Stately ${this.state.userIsIn}`}</h2>
   }
